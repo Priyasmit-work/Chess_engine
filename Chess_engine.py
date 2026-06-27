602d101efc68681f895e653e538fb908817fb8e8
Priyasmit Ganguly <priyasmit.work.ganguly@gmail.com>
2026-06-28 01:49:09 +0530

    Initial Commit and starting out

---
+++ Chess_engine.py
@@ -0,0 +1,10 @@
+def create_board():
+        empty_rows=[["--"]*8 for i in range(4)]
+        return [
+            ["bR","bN","bB","bQ","bK","bB","bN","bR"],
+            ["bP","bP","bP","bP","bP","bP","bP","bP"],
+            * empty_rows,
+            ["wP","wP","wP","wP","wP","wP","wP","wP"],
+            ["wR","wN","wB","wQ","wK","wB","wN","wR"]
+]
+    
---
+++ Chess_main.py
@@ -0,0 +1,11 @@
+import pygame as P
+import sys
+from Chess_engine import create_board
+
+WIDTH=512
+HEIGHT=512
+SQ_SIZE=WIDTH//8
+FPS=15
+
+LIGHT=(240, 217, 181)
+DARK=(181, 136, 99)
